dae8fa39d0ef454683e518307f1add365c8f3d97
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2026-03-28 14:29:47 +0500

    feat: implement IMAP for fetching and parsing emails

---
+++ email_ai/email_reader/imap_client.py
@@ -0,0 +1,46 @@
+import imaplib
+import email
+
+from email.header import decode_header
+
+EMAIL = "muhammadyusuf.yusupov201@gmail.com"
+PASSWORD = "App Password"
+
+def connect_email():
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap.login(EMAIL, PASSWORD)
+
+    return imap
+
+def fetch_emails(imap, limit=5):
+    imap.select("inbox")
+    status, messages = imap.search(None, "ALL")
+    email_ids = messages[0].split()
+
+    return email_ids[-limit:]
+
+def read_email(imap, email_id):
+    status, msg_data = imap.fetch(email_id, "(RFC822)")
+    raw_email = msg_data[0][1]
+    msg = email.message_from_bytes(raw_email)
+
+    return msg
+
+def parse_email(msg):
+    subject, encoding = decode_header(msg["Subject"])[0]
+
+    if isinstance(subject, bytes):
+        subject = subject.decode(encoding if encoding else "utf-8", errors="ignore")
+
+    body = ""
+
+    if msg.is_multipart():
+        for part in msg.walk():
+
+            if part.get_content_type() == "text/plain":
+                body = part.get_payload(decode=True).decode(errors="ignore")
+                break
+    else:
+        body = msg.get_payload(decode=True).decode(errors="ignore")
+
+    return subject, body
